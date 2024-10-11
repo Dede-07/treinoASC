2d001878dfa5792d45d7059dee4fb5e5525e7c07
Andre_Carrijo <161145988+Dede-07@users.noreply.github.com>
2024-10-11 18:00:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,55 +11,55 @@ function treinoAsc() {
                 result.innerHTML = `
                 <h2>Segunda: Peito, Ombros e Tríceps</h2>
                 <ul>
-                    <li><input type="checkbox" class="exercise"> <b>Flexões </b>  --> 4 séries de 12-15 repetições.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Flexões Diamante </b>  --> 3 séries de 10-12 repetições.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Flexão Pike </b>  --> 4 séries de 10-12 repetições.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Tríceps na Cadeira </b>  --> 3 séries de 10-15 repetições.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Prancha com Toque no Ombro </b>  --> 3 séries de 20 toques (10 de cada lado).</li>
+                    <li><input type="checkbox" class="exercise"> <b>Flexões </b>  ⇾ 4 séries de 12-15 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Flexões Diamante </b>  ⇾ 3 séries de 10-12 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Flexão Pike </b>  ⇾ 4 séries de 10-12 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Tríceps na Cadeira </b>  ⇾ 3 séries de 10-15 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Prancha com Toque no Ombro </b>  ⇾ 3 séries de 20 toques (10 de cada lado).</li>
                 </ul>`;
                 break;
             case 2:
                 result.innerHTML = `
                 <h2>Terça: Pernas e Glúteos</h2>
                 <ul>
-                    <li><input type="checkbox" class="exercise"> <b>Agachamento livre</b>  --> 4 séries de 15-20 repetições.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Afundos (Lunges)</b>  --> 4 séries de 12 repetições por perna.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Agachamento Búlgaro</b>  --> 3 séries de 10 repetições por perna.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Elevação de panturrilha</b>  --> 4 séries de 15-20 repetições.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Ponte de glúteos</b>  --> 3 séries de 15-20 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Agachamento livre</b>  ⇾ 4 séries de 15-20 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Afundos (Lunges)</b>  ⇾ 4 séries de 12 repetições por perna.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Agachamento Búlgaro</b>  ⇾ 3 séries de 10 repetições por perna.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Elevação de panturrilha</b>  ⇾ 4 séries de 15-20 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Ponte de glúteos</b>  ⇾ 3 séries de 15-20 repetições.</li>
                 </ul>`;
                 break;
             case 3:
                 result.innerHTML = `
                 <h2>Quarta: Costas e Bíceps</h2>
                 <ul>
-                    <li><input type="checkbox" class="exercise"> <b>Barra Fixa (Costas e Bíceps)</b>  --> 4 séries de 4-8 repetições.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Remada Invertida</b>  --> 4 séries de 10-12 repetições.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Rosca Direta com Mochila</b>  --> 3 séries de 10-15 repetições.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Rosca Direta Barra</b>  --> 3 séries de 10-15 repetições.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Abdominal</b>  --> 100x (50 x 50).</li>
+                    <li><input type="checkbox" class="exercise"> <b>Barra Fixa (Costas e Bíceps)</b>  ⇾ 4 séries de 4-8 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Remada Invertida</b>  ⇾ 4 séries de 10-12 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Rosca Direta com Mochila</b>  ⇾ 3 séries de 10-15 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Rosca Direta Barra</b>  ⇾ 3 séries de 10-15 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Abdominal</b>  ⇾ 100x (50 x 50).</li>
                 </ul>`;
                 break;
             case 4:
                 result.innerHTML = `
                 <h2>Quinta: Pernas e Core</h2>
                 <ul>
-                    <li><input type="checkbox" class="exercise"> <b>Agachamento salto</b>  --> 3 séries de 10-12 repetições.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Afundo Lateral</b>  --> 3 séries de 10-12 repetições por perna.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Ponte Unilateral</b>  --> 3 séries de 10-12 repetições por perna.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Abdominal Bike</b>  --> 3 séries de 20x de cada lado.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Prancha lateral</b>  --> 3 séries de 30-45s (de cada lado).</li>
+                    <li><input type="checkbox" class="exercise"> <b>Agachamento salto</b>  ⇾ 3 séries de 10-12 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Afundo Lateral</b>  ⇾ 3 séries de 10-12 repetições por perna.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Ponte Unilateral</b>  ⇾ 3 séries de 10-12 repetições por perna.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Abdominal Bike</b>  ⇾ 3 séries de 20x de cada lado.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Prancha lateral</b>  ⇾ 3 séries de 30-45s (de cada lado).</li>
                 </ul>`;
                 break;
             case 5:
                 result.innerHTML = `
                 <h2>Sexta: Full-Body</h2>
                 <ul>
-                    <li><input type="checkbox" class="exercise"> <b>Burpees</b>  --> 3 séries de 10-15 repetições.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Flexões</b>  --> 3 séries de 10-15 repetições.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Mountain Climbers</b>  --> 3 séries de 30s.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Agachamento Livre</b>  --> 3 séries de 15-20 repetições.</li>
-                    <li><input type="checkbox" class="exercise"> <b>Barra Fixa</b>  --> 3 séries de 4-8 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Burpees</b>  ⇾ 3 séries de 10-15 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Flexões</b>  ⇾ 3 séries de 10-15 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Mountain Climbers</b>  ⇾ 3 séries de 30s.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Agachamento Livre</b>  ⇾ 3 séries de 15-20 repetições.</li>
+                    <li><input type="checkbox" class="exercise"> <b>Barra Fixa</b>  ⇾ 3 séries de 4-8 repetições.</li>
                 </ul>`;
                 break;
             default:
